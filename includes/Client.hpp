79949caacef7e418c9fb20c4c3d2b884c9fc8577
Daniil <stashk9@gmail.com>
2021-09-21 11:47:16 +0300

    setter

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -12,13 +12,11 @@ private:
 		std::string				_id;
 		std::string				_host;
 		std::string				_nickname;
-		std::string				_username;
 		std::string				_firstname;
 		std::string				_message;
 		bool					_didEnter;
 		bool					_isIdentified;
 
-		static size_t		_maxChannels;
 		// std::vector<Channel *>	_channels;
 		// Channel*			_channel;
 
@@ -26,13 +24,29 @@ public:
 		Client(int sockFd, int port, Server *serv, char *host = nullptr);
 		~Client(void) {};
 
-		std::string getId(void) {return _id;};
-		bool		getIsIdentified(void) {return _isIdentified;};
-		std::string getNick(void) {return _nickname;};
-		int			getSockFd(void) {return _sockFd;};
-		std::string	getHost(void) {return _host;};
-		int			getPort(void) {return _port;};
-		std::string getMessage(void) {return _message;};
+		std::string getId(void) {return _id;}
+		bool		getIsIdentified(void) {return _isIdentified;}
+		std::string getNick(void) {return _nickname;}
+		int			getSockFd(void) {return _sockFd;}
+		std::string	getHost(void) {return _host;}
+		int			getPort(void) {return _port;}
+		std::string getMessage(void) {return _message;}
+		std::string getName(void) {return _firstname;}
+		bool		getDidEnter(void) {return _didEnter;}
+
+		void		setSockFd(int sock) {_sockFd = sock;}
+		void		setPort(int port) {_port = port;}
+		void		setId(std::string id) {_id = id;}
+		void		setHost(std::string host) {_host = host;}
+		void		setNick(std::string nick) {_nickname = nick;}
+		void		setName(std::string name) {_firstname = name;}
+		void		setDidEnter(bool i) {_didEnter = i;}
+		void		setIsIdentified(bool i) {_isIdentified = i;}
+
+
+
+
+
 		
 		void		clearMessage(void);
 		void		appendMessage(std::string message);
